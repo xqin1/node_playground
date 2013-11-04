cb39bd2954c268b35f66402b592951ef73bfbdf0
xiaoming qin <qinxm2000@yahoo.com>
2013-11-03 19:09:25 -0500

    hw2

--- mongo/mongo_driver.js
+++ mongo/mongo_driver.js
@@ -13,37 +13,37 @@ mongoClient.open(function(err,mongoClient){
 	console.log("mongo client connected");
 
 //get reddit json through  request
-	// request('http://www.reddit.com/r/technology/.json', function(err,response,body){
-	// 	if (!err && response.statusCode == 200){
-	// 		//console.log(JSON.stringify(body))
-	// 		var obj = JSON.parse(body);
-	// 		var stories = obj.data.children.map(function(story){return story.data});
-	// 		db.collection("reddit").insert(stories, function(err,data){
-	// 			if (err) throw err;
-	// 			console.log(data);
-	// 			db.close();
-	// 		})
+	request('http://www.reddit.com/r/technology/.json', function(err,response,body){
+		if (!err && response.statusCode == 200){
+			//console.log(JSON.stringify(body))
+			var obj = JSON.parse(body);
+			var stories = obj.data.children.map(function(story){return story.data});
+			db.collection("reddit").insert(stories, function(err,data){
+				if (err) throw err;
+				console.log(data);
+				db.close();
+			})
 
-	// 	}
-	// })
+		}
+	})
 
 
 //basic nodejs driver exercise
 	//var query = {"apiName":"Demographic-findByCordinates", "latlng.lat":{"$gt":41.341, "$lt": 41.342}};
-	var query = {};
-	var projection = {"domain":1, "author":1, "url":1, "_id":0};
-	var options = {"skip":1, "limit":4, "sort":[["author",1],["domain",-1]]}
+	// var query = {};
+	// var projection = {"domain":1, "author":1, "url":1, "_id":0};
+	// var options = {"skip":1, "limit":4, "sort":[["author",1],["domain",-1]]}
 
-	var cursor = db.collection("reddit").find(query,projection,options);
-	// cursor.skip(1);
-	// cursor.limit(4);
-	// cursor.sort([["author",1],["domain",-1]]);
-	cursor.each(callback);
+	// var cursor = db.collection("reddit").find(query,projection,options);
+	// // cursor.skip(1);
+	// // cursor.limit(4);
+	// // cursor.sort([["author",1],["domain",-1]]);
+	// cursor.each(callback);
 
-	function callback(err,doc){
-		if (err) throw err;
-		doc != null ? console.dir(doc) : db.close();
-	}
+	// function callback(err,doc){
+	// 	if (err) throw err;
+	// 	doc != null ? console.dir(doc) : db.close();
+	// }
 
 //use request lib re get reddit data
 	//db.close();
